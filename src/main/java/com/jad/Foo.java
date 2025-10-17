694e540ed295e288c947ae15ca57b3f3c147aef3
PierreTDX <pierre.tondeux@gmail.com>
2025-10-17 16:22:44 +0200

    feat: implement Foo with constructor, getBar() and getQux() Errors: 5 => to 3 (progress good)

--- src/main/java/com/jad/Foo.java
+++ src/main/java/com/jad/Foo.java
@@ -1,40 +1,45 @@
 package com.jad;
 
-import java.util.List;
+// import java.util.List;
 
 public class Foo {
+    private final Bar bar;
+    private final Qux qux;
+
     public Foo(final Bar bar) {
+        this.bar = bar;
+        this.qux = new Qux();
     }
 
     public Bar getBar() {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
-
-    public List<Baz> getBazs() {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
-
-    public void addBaz(final Baz baz) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return bar;
     }
 
     public Qux getQux() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return qux;
     }
 
-    public List<Grault> getGraults() {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
+    // public List<Baz> getBazs() {
+    // throw new UnsupportedOperationException("Not implemented yet");
+    // }
 
-    public void addGrault() {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
+    // public void addBaz(final Baz baz) {
+    // throw new UnsupportedOperationException("Not implemented yet");
+    // }
 
-    public Corge getCorge() {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
+    // public List<Grault> getGraults() {
+    // throw new UnsupportedOperationException("Not implemented yet");
+    // }
 
-    public void setCorge(final Corge firstCorge) {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
+    // public void addGrault() {
+    // throw new UnsupportedOperationException("Not implemented yet");
+    // }
+
+    // public Corge getCorge() {
+    // throw new UnsupportedOperationException("Not implemented yet");
+    // }
+
+    // public void setCorge(final Corge firstCorge) {
+    // throw new UnsupportedOperationException("Not implemented yet");
+    // }
 }
